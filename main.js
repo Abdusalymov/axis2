5b60a19d370bca978e118575ee34931028cd4208
Ildar Tahirovich <irbis180288@yandex.ru>
2018-06-04 19:44:00 +0500

    corrections

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let random_c = Math.floor(11 + Math.random()*4);
 let random_b = random_c - random_a;
 
 
+
 const wrapper_a = document.querySelector('.wrapper_a');
 const wrapper_b = document.querySelector('.wrapper_b');
 
@@ -15,17 +16,32 @@ box_b.innerHTML = random_b;
 
 const period1 = document.querySelector('.period1');
 const period2 = document.querySelector('.period2');
-period1.style.width = 4.85 * random_a +'%';
-period2.style.width = 4.85 * random_b +'%';
+period1.style.width = 4.89 * random_a +'%';
+period2.style.width = 4.91 * random_b +'%';
 
 
 const inputA = document.querySelector('#a');
 const inputB = document.querySelector('#b');
+inputA.onkeypress = function (e) {
+	return noString(e);
+}
+
+inputB.onkeypress = function (e) {
+	return noString(e);
+}
+
+function noString(e){
+  return !(/[А-Яа-яA-Za-z ]/.test(String.fromCharCode(e.charCode)));
+
+}
 
 let create_inputeC = document.createElement('input')
 create_inputeC.setAttribute('type', 'text');
 create_inputeC.setAttribute('maxlength', 2);
-create_inputeC.className = 'answer'
+create_inputeC.className = 'answer';
+create_inputeC.onkeypress = function (e) {
+	return noString(e);
+}
 
 inputA.addEventListener('input', function(){
 	if(inputA.value == random_a){
@@ -34,7 +50,7 @@ inputA.addEventListener('input', function(){
 		inputA.value = '';
 		wrapper_a.innerHTML = random_a;
 		period2.style.display = 'flex';
-		
+		inputB.focus();
 	}
 	else{ 
 		inputA.style.color = 'red';
@@ -50,6 +66,7 @@ inputB.addEventListener('input', function(){
 		wrapper_b.innerHTML = random_b;
 		box_c.innerHTML = '';
 		box_c.appendChild(create_inputeC);
+		create_inputeC.focus();
 	}
 	else{ 
 		inputB.style.color = 'red';
@@ -66,3 +83,9 @@ create_inputeC.addEventListener('input', function(){
 		create_inputeC.style.color = 'red';
 	}
 })
+
+function lengthSection(){
+		period1.lastElementChild.setAttribute('src', `section_${random_a}.svg`)
+		period2.lastElementChild.setAttribute('src', `section_${random_b}.svg`)
+}
+lengthSection();
